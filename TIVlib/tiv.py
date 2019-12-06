5810e790ef255d99b1057d2b9c69f8f4556d4e46
Antonio Ramires <aframires@gmail.com>
2019-12-06 23:45:26 +0100

    Fixed a bug in cosine distance

--- TIVlib/tiv.py
+++ TIVlib/tiv.py
@@ -195,5 +195,5 @@ class TIV:
     @classmethod
     def cosine(cls, tiv1, tiv2):
         tiv1_split = np.concatenate((tiv1.vector.real, tiv1.vector.imag), axis=0)
-        tiv2_split = np.concatenate((tiv1.vector.real, tiv1.vector.imag), axis=0)
+        tiv2_split = np.concatenate((tiv2.vector.real, tiv2.vector.imag), axis=0)
         return np.arccos(np.dot(tiv1_split, tiv2_split) / (np.linalg.norm(tiv1.vector) * np.linalg.norm(tiv2.vector)))
